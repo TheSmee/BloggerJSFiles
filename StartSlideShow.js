5057627a9f8d36215d5ee140a6a46b638ef5843e
TheSmee <GitHub@amezaga.us>
2021-09-28 01:07:20 -0400

    Initial Commit

---
+++ StartSlideShow.js
@@ -0,0 +1,12 @@
+/*
+ * Scroll 
+ */
+var _0xb687=["&nbsp;","write","noConflict","show","superfish","ul.menu-primary","Menu","menu-primary-responsive","menu-primary-responsive-container","&ndash;","mobileMenu",".menu-primary-container","ready","href","location","attr","current-cat","addClass","li","parent","each","#pagelistmenusblogul li a","ul.menu-secondary","Navigation","menu-secondary-responsive","menu-secondary-responsive-container",".menu-secondary-container","fitVids",".post","scrollHorz",".fp-next",".fp-prev",".fp-pager","cycle",".fp-slides"];
+jQuery.noConflict();
+jQuery(document)[_0xb687[12]](function(){jQuery(_0xb687[11])[_0xb687[10]]({defaultText:_0xb687[6],className:_0xb687[7],containerClass:_0xb687[8],subMenuDash:_0xb687[9]})});
+jQuery(document)[_0xb687[12]](function(){var _0x4618x1=window[_0xb687[14]][_0xb687[13]];
+jQuery(_0xb687[21])[_0xb687[20]](function(){var _0x4618x2=jQuery(this)[_0xb687[15]](_0xb687[13]);
+if(_0x4618x2== _0x4618x1){jQuery(this)[_0xb687[19]](_0xb687[18])[_0xb687[17]](_0xb687[16])}})});
+jQuery(function(){jQuery(_0xb687[22])[_0xb687[4]]({animation:{opacity:_0xb687[3]},autoArrows:true,dropShadows:false,speed:200,delay:800})});
+jQuery(document)[_0xb687[12]](function(){jQuery(_0xb687[26])[_0xb687[10]]({defaultText:_0xb687[23],className:_0xb687[24],containerClass:_0xb687[25],subMenuDash:_0xb687[9]});
+jQuery(_0xb687[28])[_0xb687[27]]()});
